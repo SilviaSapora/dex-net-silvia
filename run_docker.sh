abb010f8f22904168abc5ea8bae5fec54713c91b
Alberto Spina <alberto.spina15@imperial.ac.uk>
2018-11-19 16:51:01 +0000

    Dockerfile: Add ubuntu script

--- run_docker.sh
+++ run_docker.sh
@@ -1,3 +1,3 @@
 IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $IP
-docker run --privileged -it -e "DISPLAY=$IP:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" dex-net ${1:-}
+docker run -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" --privileged -it dex-net ${1:-}
