ac4e03f5e1ffff6cceed74c4c11a81d30b5f1529
ss11915 <ss11915@imperial.ac.uk>
2019-04-10 23:26:28 +0100

    scripts/: updated and renamed

--- scripts/api.sh
+++ scripts/api.sh
@@ -1,5 +1,6 @@
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib:/usr/local/lib64"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib:/usr/local/lib64:/home/silvia/dex-net/deps/meshpy/meshpy"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/freenect2/lib"
 export PYTHONPATH="/usr/local/lib/python2.7/dist-packages/vtk:$LD_LIBRARY_PATH"
+
 #python setup.py test
 python apps/dexnet_cli.py
