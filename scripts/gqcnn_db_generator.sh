e35813c7bc64de6c8c7072181e60bb9857aabec1
ss11915 <ss11915@imperial.ac.uk>
2019-04-10 15:10:49 +0100

    scripts: scripts for running gqcnn creation and visualization created

---
+++ scripts/gqcnn_db_generator.sh
@@ -0,0 +1,4 @@
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib:/usr/local/lib64:/home/silvia/dex-net/deps/meshpy/meshpy"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/freenect2/lib"
+export PYTHONPATH="/usr/local/lib/python2.7/dist-packages/vtk:$LD_LIBRARY_PATH"
+python tools/generate_gqcnn_dataset_vrep.py data/datasets/gqcnn
